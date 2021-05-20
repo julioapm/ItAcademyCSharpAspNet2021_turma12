2fea9e77bcee9b3586e84cefc8a0b85a907cff5a
Júlio Pereira Machado <julioapm@gmail.com>
2021-05-20 15:33:26 -0300

    Código inicial

---
+++ DemoEventos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace DemoEventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contador c = new Contador(3);
+            c.ValorAtingido += (origem, dados) => {
+                Console.WriteLine(origem);
+                Console.WriteLine("Bum!");
+            };
+            for(int i=0; i<5; i++)
+            {
+                c.Incrementar();
+                Console.WriteLine(c.Valor);
+            }
+            Console.WriteLine("Fim");
+        }
+    }
+
+    class Contador
+    {
+        private int valorAlvo;
+        public event EventHandler ValorAtingido;
+        public int Valor {get; private set;}
+        public Contador(int v)
+        {
+            valorAlvo = v;
+        }
+        public void Incrementar()
+        {
+            Valor++;
+            if (Valor == valorAlvo)
+            {
+                var e = EventArgs.Empty;
+                OnValorAtingido(e);
+            }
+        }
+        protected virtual void OnValorAtingido(EventArgs e)
+        {
+            if (ValorAtingido != null)
+            {
+                ValorAtingido(this, e);
+            }
+        }
+    }
+}
