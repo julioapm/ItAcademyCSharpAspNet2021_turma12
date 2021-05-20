b62b8b5e96c86b7f6b9ad3df61bd30c785470d00
Júlio Pereira Machado <julioapm@gmail.com>
2021-05-20 14:54:39 -0300

    Código inicial

---
+++ DemoDelegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DemoDelegate
+{
+    public delegate int PerformCalculation(int x, int y);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PerformCalculation refMetodo;
+            refMetodo = Somar;
+            int r = refMetodo(3,7);
+            Console.WriteLine(r);
+            refMetodo = Multiplicar;
+            r = refMetodo(3,7);
+            Console.WriteLine(r);
+            PerformCalculation refMetodos = Somar;
+            refMetodos += Multiplicar;
+            Console.WriteLine(refMetodos.GetInvocationList().GetLength(0));
+            PerformCalculation refAnonimo = delegate(int a, int b) { return a - b;};
+            PerformCalculation refLambda = (a, b) => a/b;
+            r = refLambda(5,2);
+            Console.WriteLine(r);
+            AplicarCalculo((x,y)=>x-y,10,7);
+        }
+        static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+        static int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+        static void AplicarCalculo(PerformCalculation calculation, int a, int b)
+        {
+            Console.WriteLine(calculation(a,b));
+        }
+    }
+}
---
+++ Laboratorio8cs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Laboratorio8cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> listaStrings = new List<string>();
+            listaStrings.Add("UmPalavrao");
+            listaStrings.Add("Hello");
+            listaStrings.Add("World");
+            Console.WriteLine(listaStrings.Count);
+            Console.WriteLine(listaStrings[0]);
+            foreach(var item in listaStrings)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(listaStrings.Max());
+            IList<string> outraLista = new List<string>(){"Outra", "MaisUma"};
+            var resultado = listaStrings.Concat(outraLista);
+            Console.WriteLine(listaStrings);
+            Console.WriteLine(outraLista);
+            Console.WriteLine(resultado);
+            Console.WriteLine(resultado.GetType().Name);
+            
+        }
+    }
+}
