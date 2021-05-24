5330990c36f4dc35dc26d4f90e95cb75c75bf6ad
Júlio Pereira Machado <julioapm@gmail.com>
2021-05-24 15:24:03 -0300

    Código final

---
+++ DemoAsync2/Post.cs
@@ -0,0 +1,8 @@
+namespace DemoAsync2
+{
+    public class Post
+    {
+        public string title {get;set;}
+        public int id {get;set;}
+    }
+}
---
+++ DemoAsync2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace DemoAsync2
+{
+    class Program
+    {
+        static readonly HttpClient cliente = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            string uri = "https://jsonplaceholder.typicode.com/posts";
+            try
+            {
+                /*
+                var resposta = await cliente.GetAsync(uri);
+                Console.WriteLine(resposta.StatusCode);
+                Console.WriteLine(await resposta.Content.ReadAsStringAsync());
+                */
+                var resposta = await cliente.GetFromJsonAsync<List<Post>>(uri);
+                Console.WriteLine(resposta.Count);
+                foreach(var p in resposta)
+                {
+                    Console.WriteLine(p.title);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Problemas:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
