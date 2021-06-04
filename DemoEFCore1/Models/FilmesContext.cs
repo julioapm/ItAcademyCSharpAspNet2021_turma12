3e64e7efee1873a875a1d7038ae7152e4e38e9d5
Júlio Pereira Machado <julioapm@gmail.com>
2021-06-04 15:38:06 -0300

    Configuração de logging

--- DemoEFCore1/Models/FilmesContext.cs
+++ DemoEFCore1/Models/FilmesContext.cs
@@ -25,6 +25,8 @@ namespace DemoEFCore1.Models
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=ITA12_filmes;Integrated Security=True");
+                optionsBuilder.LogTo(Console.WriteLine)
+                              .EnableSensitiveDataLogging();
             }
         }
 
