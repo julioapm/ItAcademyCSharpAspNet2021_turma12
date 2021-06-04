8fb683e00ee5e1a7d84c2f91f11e675d91cc895c
Júlio Pereira Machado <julioapm@gmail.com>
2021-06-04 15:06:34 -0300

    Código do scaffolding

---
+++ DemoEFCore1/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DemoEFCore1.Models
+{
+    public partial class Filme
+    {
+        public string Titulo { get; set; }
+        public decimal? Ano { get; set; }
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        public string AtoresPrincipais { get; set; }
+        public decimal? Duracao { get; set; }
+        public decimal? ValorIngresso { get; set; }
+    }
+}
