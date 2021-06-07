d8ffd55033c07b8cb07b9088291c25a47e61302f
Júlio Pereira Machado <julioapm@gmail.com>
2021-06-07 14:38:47 -0300

    Finalização das operações CRUD

--- DemoEFCore3/Program.cs
+++ DemoEFCore3/Program.cs
@@ -12,16 +12,35 @@ namespace DemoEFCore3
         {
             using (var bd = new BaseDadosContext())
             {
+                /*
                 Console.WriteLine("Inserindo produtos:");
                 var novoProduto = new Produto{Descricao="Novo Produto", PrecoUnitario=10.99F, EstaDisponivel=true};
                 await bd.Produtos.AddAsync(novoProduto);
                 await bd.SaveChangesAsync();
+                */
 
+                /*
+                Console.WriteLine("Alterando um produto:");
+                var umProduto = await bd.Produtos.FindAsync(4L);
+                if (umProduto != null)
+                {
+                    umProduto.PrecoUnitario = 15F;
+                    await bd.SaveChangesAsync();
+                }
+                */
+
+                Console.WriteLine("Removendo um produto:");
+                var umProduto = await bd.Produtos.FindAsync(4L);
+                bd.Remove(umProduto);
+                await bd.SaveChangesAsync();
+                
+                /*
                 Console.WriteLine("Consultando todos os produtos:");
                 var produtos = await bd.Produtos
                             .OrderBy(p => p.PrecoUnitario)
                             .ToListAsync();
                 produtos.ForEach(p => Console.WriteLine($"{p.Codigo} : {p.PrecoUnitario}"));
+                */
             }
         }
     }
