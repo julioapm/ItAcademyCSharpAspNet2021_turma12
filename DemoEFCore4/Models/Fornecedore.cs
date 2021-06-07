4177abdba6fb59240983c757d6cf2a4c6005b148
Júlio Pereira Machado <julioapm@gmail.com>
2021-06-07 15:11:48 -0300

    Setup incial com scaffold

---
+++ DemoEFCore4/Models/Fornecedore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DemoEFCore4.Models
+{
+    public partial class Fornecedore
+    {
+        public Fornecedore()
+        {
+            Produtos = new HashSet<Produto>();
+        }
+
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ DemoEFCore4/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DemoEFCore4.Models
+{
+    public partial class Produto
+    {
+        public long Codigo { get; set; }
+        public string Descricao { get; set; }
+        public float Precounitario { get; set; }
+        public bool Estadisponivel { get; set; }
+        public string CodigoFornecedor { get; set; }
+
+        public virtual Fornecedore CodigoFornecedorNavigation { get; set; }
+    }
+}
